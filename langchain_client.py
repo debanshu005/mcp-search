54bdc71cafca0eaa37f9e93c9b05d244aad11a92
Debanshu Biswas <debanshu005@gmail.com>
2025-04-28 20:01:23 +0530

    Added langchain client for MCP tools

---
+++ langchain_client.py
@@ -0,0 +1,24 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+
+from langchain_openai import ChatOpenAI
+model = ChatOpenAI(model="gpt-4o")
+
+async with MultiServerMCPClient(
+    {
+        "math": {
+            "command": "python",
+            # Make sure to update to the full absolute path to your math_server.py file
+            "args": ["/path/to/math_server.py"],
+            "transport": "stdio",
+        },
+        "weather": {
+            # make sure you start your weather server on port 8000
+            "url": "http://localhost:8000/sse",
+            "transport": "sse",
+        }
+    }
+) as client:
+    agent = create_react_agent(model, client.get_tools())
+    math_response = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
+    weather_response = await agent.ainvoke({"messages": "what is the weather in nyc?"})
